011396d89123d2740f1c3c294653d946db344b98
NPetz <souleisen@gmail.com>
2020-08-25 01:33:54 +0900

    added firebase

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,42 @@
-npm <template>
+<template>
   <div>
-    <SearchBar v-on:new-input="fetchData" /><SearchResults :prop="results" />
+    <SearchBar v-on:new-input="fetchData" />
+    <SearchResults :prop="results" @fetch-definition="fetchDefinition">
+      <article v-for="(entry, index) in japRes" :key="index">
+        <li v-html="entry.innerHTML"></li>
+      </article>
+      <article v-for="(entry, index) in engRes" :key="-index-1">
+        <li v-html="entry.innerHTML"></li>
+      </article>
+    </SearchResults>
   </div>
 </template>
 
 <script>
 import SearchBar from "./components/SearchBar.vue";
 import SearchResults from "./components/SearchResults.vue";
+
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: "adroit-autumn-237308.firebaseapp.com",
+  databaseURL: "https://adroit-autumn-237308.firebaseio.com",
+  projectId: "adroit-autumn-237308",
+  storageBucket: "adroit-autumn-237308.appspot.com",
+  messagingSenderId: "411441913058",
+  appId: "1:411441913058:web:7ddf2db626f6a036df92bb",
+  measurementId: "G-2K44H693HQ"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const parser = new DOMParser();
 
 export default {
   name: "App",
@@ -15,12 +45,14 @@ export default {
     SearchResults,
   },
 
-  data: function () {
+  data: function() {
     return {
-      results: ""
-    }
+      results: null,
+      japRes: null,
+      engRes: null,
+    };
   },
-  
+
   computed: {
     api: () => process.env.VUE_APP_API_KEY,
 
@@ -34,10 +66,56 @@ export default {
       console.log(uri);
       const response = await fetch(uri);
       const data = await response.json();
-      this.results = data.items
-      console.log(data)
+      this.results = data.items;
+      console.log(data);
+    },
+
+    async fetchDefinition(e) {
+      this.fetchEng(e.title);
+
+      this.fetchJap(e.link);
+    },
+
+    async fetchJap(url) {
+      console.log("fetching definitionfrom:", url);
+
+      const response = await fetch(
+        `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`
+      );
+      const data = await response.json();
+      const htmlData = parser
+        .parseFromString(data.contents, "text/html")
+        .getElementById("mainArea")
+        .querySelectorAll("article section.description");
+      this.japRes = htmlData ? htmlData : "No Results";
+      console.log(data, this.japRes);
+    },
+
+    async fetchEng(term) {
+      console.log("fethcing englsih defnitnon", term);
+      let match = null;
+
+      try {
+        match = term.match(/(.*)\(/i)[1];
+      } catch (e) {
+        console.log("no match", e);
+      }
+      term = match ? match : term.replace(/とは\s-\s.*/g, "");
+      console.log("fetching for term:", term);
+      const uri = await `http://nihongo.monash.edu/cgi-bin/wwwjdic?1ZUJ${encodeURIComponent(
+        term
+      )}`;
+      const response = await fetch(
+        `https://api.allorigins.win/get?url=${encodeURIComponent(uri)}`
+      );
+      const data = await response.json();
+      const parsed = await parser
+        .parseFromString(data.contents, "text/html")
+        .querySelector("pre");
+      this.engRes = [parsed];
+
+      console.log("parsed value: ", parsed);
     },
   },
 };
 </script>
-
--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -1,13 +1,31 @@
 <template>
-  <ul>
-    <li v-for="(entry, index) in prop" :key="index"> <a :href="entry.formattedUrl" target="__blank">{{entry.htmlTitle}}</a> </li>
-  </ul>
-</template>
+  <main>
+    <ol>
+      <li v-for="(entry, index) in prop" :key="index">
+        <p @click="$emit('fetch-definition', entry)" target="__blank">
+          {{ parseResults(entry.title) }}
+        </p>
+      </li>
+    </ol>
 
+    <section>
+      <slot></slot>
+    </section>
+  </main>
+</template>
 
 <script>
 export default {
   name: "SearchResults",
   props: ["prop"],
+  methods: {
+    parseResults(text) {
+      console.log("original text = ", text);
+      //
+      const endString = /とは\s-\s.*/g;
+
+      return text.replace(endString, "");
+    },
+  },
 };
 </script>
